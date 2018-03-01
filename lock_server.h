4cc55390e8c2b3eb7cd21bb18445f36f1feddb56
j2payton <payton@temple.edu>
2018-03-01 12:59:56 -0500

    Added lock client/server solution for HW1

--- lock_server.h
+++ lock_server.h
@@ -7,23 +7,28 @@
 #include <string>
 #include "lock_protocol.h"
 #include "lock_client.h"
-#include "locks.h"
 #include "rpc.h"
+#include <map>
+#include <pthread.h>
 
 class lock_server {
 
-  protected:
-    int nacquire;
-    locks locks_;
+    protected:
+        int nacquire;
+        enum lock_state { LOCKFREE, LOCKED };
+        typedef int l_state;
+        typedef std::map<lock_protocol::lockid_t, l_state> TLockStateMap;
+        TLockStateMap tLockMap;
+        pthread_mutex_t lmap_mutex;
+        pthread_cond_t lmap_state_cv;
 
-  public:
-    lock_server();
 
-    ~lock_server() { };
-
-    lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
-    lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &r);
-    lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &r);
+    public:
+        lock_server();
+        ~lock_server();
+        lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+        lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+        lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 
 };
 
